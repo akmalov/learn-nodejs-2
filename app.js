3aa53260634ec5018d6e9d3b371e836670284608
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-04 19:53:56 +0300

    configure callbacks in separate file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode');
+
 const argv = yargs
     .options({
         a: {
@@ -14,17 +16,10 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-let encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}',
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find that address.');
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`)
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
